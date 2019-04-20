334559d7ba4510613bcbb3f2350c62a1d21d7acf
charanyachinnasamy <charanyachinnasamy@gmail.com>
2019-04-19 18:53:18 -0700

    checking in full project

--- Login.py
+++ Login.py
@@ -1,12 +1,21 @@
-
 import time
 from selenium import webdriver
 
 appBusURL = 'https://dev.e-dapt.net:5554/edapt-admin/login.jsp'
 try:
 
-    driver = webdriver.Chrome(executable_path="chromedriver")
+    driver = webdriver.Chrome(executable_path="chromedriver.exe")
     driver.get(appBusURL)
+    driver.switch_to.window(driver.window_handles[-1])
+    elem = driver.title
+    if elem == "Select a certificate":
+        driver.find_element_by_id("btnId").Click()
+    else:
+        driver.quit()
+
+
+
+
 
     driver.find_element_by_xpath("//input[@name ='username']").send_keys('edapt-setup')
     driver.find_element_by_xpath("//input[@name = 'password']").send_keys('511maps')
