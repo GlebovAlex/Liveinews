935be31771b86a523c7fcc91366d6ed7c456764b
Mary P <thy.mary.p@gmail.com>
2019-04-23 11:21:18 -0700

    CJ-web-login and CJ-web-reg files

--- CJ-web-login.py
+++ CJ-web-login.py
@@ -6,22 +6,28 @@ from selenium import webdriver
 class Login(unittest.TestCase):
     def setUp(self):
         # create a new Chrome session
-        self.driver = webdriver.Chrome(executable_path="chromedriver")
-        self.driver.implicitly_wait(30)
-        self.driver.maximize_window()
+        global driver
+        options = Options()
+        options.headless = True
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+        options.binary_location = '/usr/bin/google-chrome'
+        driver = webdriver.Chrome(options=options, executable_path='/usr/bin/chromedriver')
+        driver.implicitly_wait(30)
+        driver.maximize_window()
         # navigate to the application home page
-        self.driver.get("http://liveinews.com/login/")
+        driver.get("http://liveinews.com/login/")
 
     def test_cred_login(self):
         #login to the LIVEiNEWS url
-        self.driver.find_element_by_name('log').send_keys('u3.qallab')
-        self.driver.find_element_by_name('pwd').send_keys('Test@1234')
-        self.driver.find_element_by_name("wp-submit").click()
+        driver.find_element_by_name('log').send_keys('u3.qallab')
+        driver.find_element_by_name('pwd').send_keys('Test@1234')
+        driver.find_element_by_name("wp-submit").click()
         time.sleep(4)
 
     def tearDown(self):
         # close the browser window
-        self.driver.quit()
+        driver.quit()
 
 if __name__ == '__main__':
     unittest.main()
--- CJ-web-reg-Chrome.py
+++ CJ-web-reg-Chrome.py
@@ -9,47 +9,54 @@ from selenium.webdriver.common.keys import Keys
 class Registration(unittest.TestCase):
     def setUp(self):
         # create a new Chrome session
-        self.driver = webdriver.Chrome(executable_path="chromedriver")
-        self.driver.implicitly_wait(30)
-        self.driver.maximize_window()
+        global driver
+        options = Options()
+        options.headless = True
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+        options.binary_location = '/usr/bin/google-chrome'
+        driver = webdriver.Chrome(options=options, executable_path='/usr/bin/chromedriver')
+        driver.implicitly_wait(30)
+        driver.maximize_window()
         # navigate to the application home page
-        self.driver.get("http://liveinews.com/login/")
+        driver.get("http://liveinews.com/login/")
 
     def test_web_registeration(self):
         # enter the registration details
-        select_role = Select(self.driver.find_element_by_id("i-e-type"))
+        select_role = Select(driver.find_element_by_id("i-e-type"))
         select_role.select_by_visible_text("Citizen Journalist")
-        self.driver.find_element_by_id("i-e-name").send_keys("u3.qallab")
-        self.driver.find_element_by_id("reg_password").send_keys("Test@1234")
-        self.driver.find_element_by_id("reg_email").send_keys("u3.qallab@gmail.com")
-        self.driver.find_element_by_id("creg_password").send_keys("Test@1234")
-        self.driver.find_element_by_id("i-e-name").send_keys("u3.qallab")
-        self.driver.find_element_by_id("i-e-first").send_keys("u3")
-        self.driver.find_element_by_id("i-e-last").send_keys("qallab")
-        self.driver.find_element_by_id("i-e-address").send_keys("123 apt 123")
-        self.driver.find_element_by_id("i-e-phone").send_keys("123-456-7890")
-        self.driver.find_element_by_id("i-e-zip").send_keys("95008")
-        select_country = Select(self.driver.find_element_by_id("countryId"))
+        driver.find_element_by_id("i-e-name").send_keys("u3.qallab")
+        driver.find_element_by_id("reg_password").send_keys("Test@1234")
+        driver.find_element_by_id("reg_email").send_keys("u3.qallab@gmail.com")
+        driver.find_element_by_id("creg_password").send_keys("Test@1234")
+        driver.find_element_by_id("i-e-name").send_keys("u3.qallab")
+        driver.find_element_by_id("i-e-first").send_keys("u3")
+        driver.find_element_by_id("i-e-last").send_keys("qallab")
+        driver.find_element_by_id("i-e-address").send_keys("123 apt 123")
+        driver.find_element_by_id("i-e-phone").send_keys("123-456-7890")
+        driver.find_element_by_id("i-e-zip").send_keys("95008")
+        select_country = Select(driver.find_element_by_id("countryId"))
         select_country.select_by_visible_text("USA")
-        select_state = Select(self.driver.find_element_by_id("stateId"))
+        select_state = Select(driver.find_element_by_id("stateId"))
         select_state.select_by_visible_text("California")
-        select_city = Select(self.driver.find_element_by_id("cityId"))
+        select_city = Select(driver.find_element_by_id("cityId"))
         select_city.select_by_visible_text("San Jose, Santa Clara County")
-        self.driver.find_element_by_id("i-e-paypal").send_keys("u3.qallab@gmail.com")
-        self.driver.find_element_by_name("register").click()
-        element = self.driver.find_element_by_id('acceptterms')
+        driver.find_element_by_id("i-e-paypal").send_keys("u3.qallab@gmail.com")
+        driver.find_element_by_name("register").click()
+        # accept the Terms and Conditions
+        element = driver.find_element_by_id('acceptterms')
         element.send_keys(Keys.END)
         time.sleep(4)
-        self.driver.find_element_by_id("acceptterms").click()
-        self.driver.find_element_by_xpath(
+        driver.find_element_by_id("acceptterms").click()
+        driver.find_element_by_xpath(
             "(.//*[normalize-space(text()) and normalize-space(.)='Terms of Service'])[1]/following::span[1]").click()
         time.sleep(4)
-        self.driver.find_element_by_name("wp-submit").click()
+        # submit the registration
+        driver.find_element_by_name("wp-submit").click()
 
-
-def tearDown(self):
+    def tearDown(self):
         # close the browser window
-        self.driver.quit()
+        driver.quit()
 
 if __name__ == '__main__':
     unittest.main()
