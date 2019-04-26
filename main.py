e6f1e6c26a7b76cd2234d7df33e22a6af9610d54
Mary P <thy.mary.p@gmail.com>
2019-04-26 11:04:34 -0700

    adding the latest changes for main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import subprocess
+subprocess.call("./Test02.py")
+subprocess.call("./CJ-web-login.py")
+subprocess.call("./CJ-web-reg-Chrome.py")
+subprocess.call("./CJ-web-login-negative.py")
